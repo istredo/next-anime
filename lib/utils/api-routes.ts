02c0171086a785bd2320bba4d8a7d6c7b2ea35e0
istredo <uvarov6799@gmail.com>
2024-04-03 12:06:03 +0300

    fix: update shuffle func

--- lib/utils/api-routes.ts
+++ lib/utils/api-routes.ts
@@ -1,4 +1,4 @@
-import { Db, MongoClient, ObjectId } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
 import { shuffle } from './commonFunc'
 
 
@@ -20,9 +20,9 @@ export const getDbAndReqBody = async (
 export const getNewAndBestsellerGoods = async (db: Db, fieldName: string) => {
 	const clothes = await db.collection('cloth').find().toArray()
 	const souvenirs = await db.collection('souvenirs').find().toArray()
-	const shuffleCloth = shuffle([...clothes].filter((item) => item[fieldName] && Object.values(item.sizes).some((value) => value))).slice(0, 2)
-	const shuffleSouvenirs = shuffle([...souvenirs].filter((item) => item[fieldName])).slice(0, 1)
+	const shuffleCloth = shuffle([...clothes].filter((item) => item[fieldName] && Object.values(item.sizes).some((value) => value))).slice(0, 3)
+	const shuffleSouvenirs = shuffle([...souvenirs].filter((item) => item[fieldName])).slice(0, 2)
 	const shuffleAd = shuffle([...clothes].filter((item) => item.characteristics.collection === 'line')).slice(0, 1)
-	const superShuffle = shuffle([...shuffleCloth, ...shuffleSouvenirs, ...shuffleAd])
+	const superShuffle = shuffle([...shuffleCloth, ...shuffleSouvenirs, ...shuffleAd]).slice(0, 4)
 	return superShuffle
 }
